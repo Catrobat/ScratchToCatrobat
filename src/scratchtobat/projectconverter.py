0fe2962577cea179fd69f366a4208b52c3bdd108
chm0815 <christoph_m@gmx.net>
2013-05-29 12:50:56 +0200

    fixed errors in projectconverter

--- src/scratchtobat/projectconverter.py
+++ src/scratchtobat/projectconverter.py
@@ -2,8 +2,8 @@ import os
 import shutil
 import zipfile
 
-from scratchreader import ScratchReader
-from scratchreader import ScratchReaderException 
+#from scratchreader import ScratchReader
+#from scratchreader import ScratchReaderException 
 from catrobatwriter import CatrobatWriter
 
 PROJECT_JSON_FILE = "project.json"
@@ -19,25 +19,25 @@ class ProjectConverter(object):
         self.sound_out_dir = os.path.join(output_folder, "sounds")
     
     def create_structure(self):
-        open(os.path.join(output_folder, ".nomedia"), 'w').close()        
-        os.mkdir(image_out_dir)
+        open(os.path.join(self.output_folder, ".nomedia"), 'w').close()        
+        os.mkdir(self.image_out_dir)
         open(os.path.join(self.image_out_dir, ".nomedia"), 'w').close()
         os.mkdir(self.sound_out_dir)
         open(os.path.join(self.sound_out_dir, ".nomedia"), 'w').close()
         #todo screenshot
     
     def extract_sb2(self):
-        with ZipFile(self.input_sb2, 'r') as myzip: 
+        with zipfile.ZipFile(self.input_sb2, 'r') as myzip: 
             myzip.extractall(self.working_dir)
     
-    def convert():
+    def convert(self):
         self.extract_sb2()
         self.create_structure()
         json_file = os.path.join(self.working_dir, PROJECT_JSON_FILE)
-        scratch_reader = ScratchReader(json_file)
+        #scratch_reader = ScratchReader(json_file)
         json_dict = self.scratch_reader.get_dict()
         
-        catrobat_writer = CatrobatWriter(json_dict, output_folder)
+        catrobat_writer = CatrobatWriter(json_dict, self.output_folder)
         catrobat_writer.process_dict()
         
         xml_string = catrobat_writer.document.toprettyxml()

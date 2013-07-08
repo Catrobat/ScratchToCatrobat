ea9b79b8f92b363b3808137675e334eb26b923a5
Christian Wutte <christian.wutte@student.tugraz.at>
2013-07-08 07:05:10 +0200

    adding catrobat helper class; update and rename of catrobat class jar

---
+++ src/scratchtobat/catrobat_util.py
@@ -0,0 +1,19 @@
+import org.catrobat.catroid.common  as catcommon
+
+BACKGROUND_SPRITE_NAME = "Hintergrund"
+
+
+def create_lookdata(name, file_name):
+    look_data = catcommon.LookData()
+    look_data.setLookName(name)
+    look_data.setLookFilename(file_name)
+    return look_data
+
+
+def background_sprite_of_project(project):
+    if project.getSpriteList().size() > 0:
+        sprite = project.getSpriteList().get(0)
+        assert sprite.getName() == BACKGROUND_SPRITE_NAME
+    else:
+        sprite = None
+    return sprite

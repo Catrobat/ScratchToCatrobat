6b3d6859059c369f069a00b37dc0870e5c01eb4b
chm0815 <christoph_m@gmx.net>
2013-07-10 21:57:39 +0200

    environment linux

---
+++ web/env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export S2C_HOME=C:\Users\christoph\develop\chwttest\ScratchToCatrobat\
+
+export JYTHONPATH=$S2C_HOME:$S2C_HOMEsrc\scratchtobat:$S2C_HOMElib\catroid_class_hierachy.jar:$S2C_HOMElib\xmlpull-1.1.3.1.jar:$S2C_HOMElib\xpp3_min-1.1.4c.jar:$S2C_HOMElib\xstream-1.4.4.jar:$S2C_HOMEsrc:$S2C_HOMElib\batik-1.7\batik-rasterizer.jar:$S2C_HOMElib\imgscalr-lib-4.2.jar

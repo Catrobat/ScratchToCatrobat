773cd9637de4be1075e6ea444af646d5521e26f9
root <root@catrobat-scratch2.ist.tugraz.at>
2013-07-10 16:49:59 -0500

    environment for debian

--- web/env.sh
+++ web/env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
+export S2C_HOME=/var/www/ScratchToCatrobat
 
-export S2C_HOME=C:\Users\christoph\develop\chwttest\ScratchToCatrobat\
+export JYTHONPATH=$S2C_HOME:$S2C_HOME/src/scratchtobat:$S2C_HOME/lib/catroid_class_hierachy.jar:$S2C_HOME/lib/xmlpull-1.1.3.1.jar:$S2C_HOME/lib/xpp3_min-1.1.4c.jar:$S2C_HOME/lib/xstream-1.4.4.jar:$S2C_HOME/src:$S2C_HOME/lib/batik-1.7/batik-rasterizer.jar:$S2C_HOME/lib/imgscalr-lib-4.2.jar:$S2C_HOME/lib/catrobat_class_hierachy.jar
 
-export JYTHONPATH=$S2C_HOME:$S2C_HOMEsrc\scratchtobat:$S2C_HOMElib\catroid_class_hierachy.jar:$S2C_HOMElib\xmlpull-1.1.3.1.jar:$S2C_HOMElib\xpp3_min-1.1.4c.jar:$S2C_HOMElib\xstream-1.4.4.jar:$S2C_HOMEsrc:$S2C_HOMElib\batik-1.7\batik-rasterizer.jar:$S2C_HOMElib\imgscalr-lib-4.2.jar

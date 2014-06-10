71ce0c57939bbce84c7b16031e630e22b97023e9
Christian Wutte <christian.wutte@student.tugraz.at>
2014-06-10 19:38:55 +0200

    improved error handling, logging; cleaning

--- src/scratchtobat/main.py
+++ src/scratchtobat/main.py
@@ -1,6 +1,9 @@
 import logging
+import os
 import sys
 import tempfile
+from java.io import IOError
+from java.lang import System
 
 from scratchtobat import common
 from scratchtobat import sb2
@@ -26,8 +29,7 @@ def scratchtobat_main(argv):
     def check_environment_settings():
         if not "java" in sys.platform:
             common.ScratchtobatError("Must be called with Jython interpreter. Aborting.")
-        from java.lang import System
-        if System.getProperty("python.security.respectJavaAccessibility") == 'true':
+        if System.getProperty("python.security.respectJavaAccessibility") != 'false':
             common.ScratchtobatError("Jython registry property 'python.security.respectJavaAccessibility' must be set to 'false'. Aborting.")
 
     check_environment_settings()
@@ -36,19 +38,21 @@ def scratchtobat_main(argv):
         usage()
 
     scratch_project_file_or_url, output_dir = argv
-    temp_download_dir = tempfile.mkdtemp()
     try:
+        if not os.path.isdir(output_dir):
+            raise EnvironmentError("Output folder must be a directory, but is %s" % output_dir)
+        working_dir = tempfile.mkdtemp()
         if scratch_project_file_or_url.startswith("http://"):
             log.info("Downloading project from URL: '{}' ...".format(scratch_project_file_or_url))
-            sb2webapi.download_project(scratch_project_file_or_url, temp_download_dir)
+            sb2webapi.download_project(scratch_project_file_or_url, working_dir)
         else:
             log.info("Extracting project from path: '{}' ...".format(scratch_project_file_or_url))
-            sb2extractor.extract_project(scratch_project_file_or_url, temp_download_dir)
-        project = sb2.Project(temp_download_dir)
-        log.info("Converting ...")
+            sb2extractor.extract_project(scratch_project_file_or_url, working_dir)
+        project = sb2.Project(working_dir)
+        log.info("Converting scratch project %s into output folder: %s", project, output_dir)
         sb2tocatrobat.convert_sb2_project_to_catrobat_zip(project, output_dir)
-    except Exception as e:
-        log.exception(e)
+    except (common.ScratchtobatError, EnvironmentError, IOError) as e:
+        log.error(e)
         return EXIT_FAILURE
     return EXIT_SUCCESS
 

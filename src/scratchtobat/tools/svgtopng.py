96f0a8632962349c2a6b7b66cef579d7811339d3
Christian Wutte <christian.wutte@student.tugraz.at>
2014-06-11 09:29:12 +0200

    process output redirected

--- src/scratchtobat/tools/svgtopng.py
+++ src/scratchtobat/tools/svgtopng.py
@@ -13,16 +13,19 @@ log = logging.getLogger(__name__)
 
 def convert(input_svg_path):
     if _BATIK_ENVIRONMENT_HOME not in os.environ:
-        log.info(os.environ)
         raise common.ScratchtobatError("Please create environment variable '{}' and set to batik library location.".format(_BATIK_ENVIRONMENT_HOME))
     batik_jar_path = os.path.join(os.environ[_BATIK_ENVIRONMENT_HOME], _BATIK_CLI_JAR)
     if not os.path.exists(batik_jar_path):
         raise common.ScratchtobatError("Jar not found: '{}'. Place batik library at {}.".format(batik_jar_path, os.path.dirname(batik_jar_path)))
     if not isinstance(input_svg_path, (str, unicode)):
         raise common.ScratchtobatError("Input argument must be str or unicode.")
+
     output_png_path = input_svg_path.replace(".svg", ".png")
-    on_error = subprocess.call(['java', '-jar', batik_jar_path, input_svg_path, '-scriptSecurityOff'])
-    if on_error:
-        raise common.ScratchtobatError("Extern jar call failed. See piped output.")
-    assert os.path.exists(output_png_path)
+    try:
+        subprocess.check_output(['java', '-jar', batik_jar_path, input_svg_path, '-scriptSecurityOff'], stderr=subprocess.STDOUT)
+        # assert os.path.exists(output_png_path)
+    except subprocess.CalledProcessError, e:
+        assert e.output
+        raise EnvironmentError("PNG to SVG conversion call failed:\n%s" % e.output)
+
     return output_png_path

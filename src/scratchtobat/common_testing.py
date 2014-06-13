27888c73904ef979c0a8977275ab1d5a2e8d2697
Christian Wutte <christian.wutte@student.tugraz.at>
2014-06-13 17:01:26 +0200

    fixed testresult folder for nested tests

--- src/scratchtobat/common_testing.py
+++ src/scratchtobat/common_testing.py
@@ -56,7 +56,7 @@ class BaseTestCase(unittest.TestCase):
     def setUp(self):
         super(BaseTestCase, self).setUp()
         self.temp_dir = tempfile.mkdtemp()
-        _package_name, _module_name, class_name, testcase_name = self.id().split(".", 3)
+        class_name, testcase_name = self.id().split(".")[-2:]
         assert class_name is not None and testcase_name is not None
         self.__testresult_base_path = os.path.join(common.get_project_base_path(), "testresult", class_name, testcase_name)
         if os.path.exists(self.__testresult_base_path):

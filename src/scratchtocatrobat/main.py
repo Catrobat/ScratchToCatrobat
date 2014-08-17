2a2d7976a8d7e670125976402d1947eb84207b35
Christian Wutte <christian.wutte@student.tugraz.at>
2014-08-17 17:22:16 +0200

    adding command-line interface, adding support for conversion from folder

--- src/scratchtocatrobat/main.py
+++ src/scratchtocatrobat/main.py
@@ -20,7 +20,9 @@
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import logging
 import os
+import shutil
 import sys
+from docopt import docopt
 from java.io import IOError
 from java.lang import System
 
@@ -35,14 +37,8 @@ EXIT_SUCCESS = 0
 EXIT_FAILURE = 1
 
 
-def scratchtobat_main(argv):
-    log.info("Called with args: '{}'".format(argv))
-
-    def usage():
-        print "usage: jython main.py <scratch-file or project url to be converted> <output_dir>"
-        print "Example 1: jython main.py http://scratch.mit.edu/projects/10205819/ output"
-        print "Example 2: jython main.py dancing_castle.scratch output"
-        sys.exit(EXIT_FAILURE)
+def run_converter(arguments):
+    log.info("Called with args: '{}'".format(arguments))
 
     def check_environment_settings():
         if "java" not in sys.platform:
@@ -52,28 +48,48 @@ def scratchtobat_main(argv):
 
     check_environment_settings()
 
-    if len(argv) != 2:
-        usage()
-
-    scratch_project_file_or_url, output_dir = argv
+    scratch_project_file_or_url, output_dir, extract_resulting_catrobat = arguments["<project-url-or-package-path>"], arguments["<output-dir>"], arguments["--extracted"]
+    temp_rm = not arguments["--no-temp-rm"]
     try:
         if not os.path.isdir(output_dir):
             raise EnvironmentError("Output folder must be a directory, but is %s" % output_dir)
-        with common.TemporaryDirectory() as scratch_project_dir:
+        with common.TemporaryDirectory(remove_on_exit=temp_rm) as scratch_project_dir:
             if scratch_project_file_or_url.startswith("http://"):
-                log.info("Downloading project from URL: '{}' ...".format(scratch_project_file_or_url))
+                log.info("Downloading project from URL: '{}' to temp dir {} ...".format(scratch_project_file_or_url, scratch_project_dir))
                 scratchwebapi.download_project(scratch_project_file_or_url, scratch_project_dir)
-            else:
+            elif os.path.isfile(scratch_project_file_or_url):
                 log.info("Extracting project from path: '{}' ...".format(scratch_project_file_or_url))
                 common.extract(scratch_project_file_or_url, scratch_project_dir)
-            scratch_project = scratch.Project(scratch_project_dir)
-            log.info("Converting scratch project %s into output folder: %s", scratch_project, output_dir)
-            converter.save_as_catrobat_program_package_to(scratch_project, output_dir)
+            else:
+                log.info("Loading project from path: '{}' ...".format(scratch_project_file_or_url))
+                scratch_project_dir = scratch_project_file_or_url
+            project = scratch.Project(scratch_project_dir)
+            log.info("Converting scratch project '%s' into output folder: %s", project.name, output_dir)
+            catrobat_program_path = converter.save_as_catrobat_program_package_to(project, output_dir)
+            if extract_resulting_catrobat:
+                extraction_path = os.path.join(output_dir, os.path.splitext(os.path.basename(catrobat_program_path))[0])
+                if os.path.exists(extraction_path):
+                    shutil.rmtree(extraction_path)
+                common.makedirs(extraction_path)
+                common.extract(catrobat_program_path, extraction_path)
     except (common.ScratchtobatError, EnvironmentError, IOError) as e:
-        log.error(e)
+        log.exception(e)
+        return EXIT_FAILURE
+    except Exception, e:
+        log.exception(e)
         return EXIT_FAILURE
     return EXIT_SUCCESS
 
 if __name__ == '__main__':
     log = logging.getLogger("scratchtocatrobat.main")
-    sys.exit(scratchtobat_main(sys.argv[1:]))
+    usage = '''ScratchToCatrobat converter
+
+    Usage:
+      'main.py' <project-url-or-package-path> <output-dir> [--extracted] [--no-temp-rm]
+
+    Options:
+      -h --help     Show this screen.
+      -e --extracted    Extract resulting Catrobat program in output-dir.
+    '''
+    arguments = docopt(usage)
+    sys.exit(run_converter(arguments))

885cdf0b92e5cf3e689d5eb0642b85bd7c95d7fa
Christian Wutte <christian.wutte@student.tugraz.at>
2014-08-18 19:08:34 +0200

    fixed class hierarchy
    * changed: Object superclass for RawProject
    * removed: unnecessary code
    * added: error handling

--- src/scratchtocatrobat/scratch.py
+++ src/scratchtocatrobat/scratch.py
@@ -20,13 +20,13 @@
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from __future__ import print_function
 
-import copy
+import collections
 import glob
 import itertools
 import json
 import os
 import sys
-import zipfile
+import urllib2
 
 from scratchtocatrobat import common
 from scratchtocatrobat import scratchwebapi
@@ -37,16 +37,8 @@ _PROJECT_FILE_NAME = "project.json"
 
 HTTP_PROJECT_URL_PREFIX = "http://scratch.mit.edu/projects/"
 
-PROJECT_REQUIRED_KEYS = ["objName", "info", "currentCostumeIndex", "penLayerMD5", "tempoBPM", "videoAlpha", "children", "costumes"]
-SCRIPT_GREEN_FLAG, SCRIPT_RECEIVE, SCRIPT_KEY_PRESSED, SCRIPT_SENSOR_GREATER_THAN, SCRIPT_SCENE_STARTS, SCRIPT_CLICKED = SCRIPTS = \
-    ["whenGreenFlag", "whenIReceive", "whenKeyPressed", "whenSensorGreaterThan", "whenSceneStarts", "whenClicked", ]
-STAGE_OBJECT_NAME = "Stage"
-STAGE_WIDTH_IN_PIXELS = 480
-STAGE_HEIGHT_IN_PIXELS = 360
-
 
 class JsonKeys(object):
-    # TODO: remove '_KEY' suffix
     BASELAYER_ID = "baseLayerID"
     CHILDREN = "children"
     COSTUME_MD5 = "baseLayerMD5"
@@ -63,38 +55,68 @@ class JsonKeys(object):
     SOUNDS = "sounds"
     VARIABLES = 'variables'
 
+PROJECT_SPECIFIC_KEYS = ["info", "currentCostumeIndex", "penLayerMD5", "tempoBPM", "videoAlpha", "children"]
+SCRIPT_GREEN_FLAG, SCRIPT_RECEIVE, SCRIPT_KEY_PRESSED, SCRIPT_SENSOR_GREATER_THAN, SCRIPT_SCENE_STARTS, SCRIPT_CLICKED = SCRIPTS = \
+    ["whenGreenFlag", "whenIReceive", "whenKeyPressed", "whenSensorGreaterThan", "whenSceneStarts", "whenClicked", ]
+STAGE_OBJECT_NAME = "Stage"
+STAGE_WIDTH_IN_PIXELS = 480
+STAGE_HEIGHT_IN_PIXELS = 360
 
 
+# TODO: rename
+class Object(common.DictAccessWrapper):
 
-class RawProject(common.DictAccessWrapper):
+    def __init__(self, object_data):
+        if not self.is_valid_class_input(object_data):
+            raise ObjectError("Input is no valid Scratch object.")
+        for key in (JsonKeys.SOUNDS, JsonKeys.COSTUMES, JsonKeys.SCRIPTS, JsonKeys.VARIABLES):
+            if key not in object_data:
+                object_data[key] = []
+        super(Object, self).__init__(object_data)
+        self.scripts = [Script(_) for _ in self.get_scripts() if Script.is_valid_script_input(_)]
+        number_of_ignored_scripts = len(self.get_scripts()) - len(self.scripts)
+        if number_of_ignored_scripts > 0:
+            _log.debug("Ignored %s scripts", number_of_ignored_scripts)
+
+    @classmethod
+    def is_valid_class_input(cls, object_data):
+        return 'objName' in object_data
+
+    def is_stage(self):
+        # TODO: extend and consolidate with verify in RawProject
+        return self.get_info() is not None
+
+    def __iter__(self):
+        return iter(self.scripts)
+
+
+class RawProject(Object):
     """
     Represents the raw Scratch project structure.
     """
 
     def __init__(self, dict_, data_origin="<undefined>"):
         super(RawProject, self).__init__(dict_)
+        assert self.is_stage()
         self._verify_scratch_dictionary(dict_, data_origin)
+        self.dict_ = dict_
         self.raw_objects = [child for child in self.get_children() if "objName" in child]
-        self.objects = [Object(raw_object) for raw_object in [self._raw_stage_object_without_project_variables(dict_)] + self.raw_objects]
-        self.stage_object = self.objects[0]
-        assert self.stage_object.get_info() is not None
-        self.nonstage_objects = self.objects[1:]
+        self.objects = [Object(raw_object) for raw_object in [dict_] + self.raw_objects]
         self.resource_names = {self._resource_name_from(raw_resource) for raw_resource in self._raw_resources()}
 
+    def __iter__(self):
+        return iter(self.objects)
+
     def _verify_scratch_dictionary(self, dict_, data_origin):
+        if self.contains_info():
+            data_origin = self.get_info().get("projectID")
         # FIXME: check which tags are really required
-        for key in PROJECT_REQUIRED_KEYS:
+        for key in PROJECT_SPECIFIC_KEYS:
             if key not in dict_:
                 raise UnsupportedProjectFileError("In project file from: '{}' key='{}' must be set.".format(data_origin, key))
 
-    def _raw_stage_object_without_project_variables(self, dict_):
-        raw_stage_object = copy.deepcopy(dict_)
-        # Variables defined on the stage object are in fact project variables, so removing them here.
-        raw_stage_object["variables"] = []
-        return raw_stage_object
-
     def _raw_resources(self):
-        return itertools.chain.from_iterable(object_.get_sounds() + object_.get_costumes() for object_ in self.objects)
+        return itertools.chain(*(object_.get_sounds() + object_.get_costumes() for object_ in self.objects))
 
     def _resource_name_from(self, raw_resource):
         assert JsonKeys.SOUND_MD5 in raw_resource or JsonKeys.COSTUME_MD5 in raw_resource
@@ -114,10 +136,15 @@ class RawProject(common.DictAccessWrapper):
         return cls(cls.raw_project_code_from_project_folder_path(project_folder_path))
 
     @classmethod
-    def from_project_code_content(cls, code_content):
-        return cls(json.loads(code_content))
+    def from_project_code_content(cls, code_content, origin="<undefined>"):
+        try:
+            parsed_json = json.loads(code_content)
+        except ValueError, e:
+            raise ProjectCodeError("No or corrupt JSON (%s): %s" % (origin, e))
+        return cls(parsed_json)
 
 
+# FIXME: do not inherit from RawProject
 class Project(RawProject):
     """
     Represents a complete Scratch project including all resource files.
@@ -126,7 +153,8 @@ class Project(RawProject):
     def __init__(self, project_base_path, name=None, id_=None):
         def read_md5_to_resource_path_mapping():
             md5_to_resource_path_map = {}
-            for project_file_path in glob.glob(os.path.join(project_base_path, "*")):
+            # TODO: clarify that only files with extension are covered
+            for project_file_path in glob.glob(os.path.join(project_base_path, "*.*")):
                 resource_name = common.md5_hash(project_file_path) + os.path.splitext(project_file_path)[1]
                 md5_to_resource_path_map[resource_name] = project_file_path
             try:
@@ -157,9 +185,14 @@ class Project(RawProject):
             self.name = name
             self.description = ""
         else:
-            self.name = scratchwebapi.request_project_name_for(self.project_id)
-            self.description = scratchwebapi.request_project_description_for(self.project_id)
-        # TODO: move whole block including the two functions to ProjectCode
+            # FIXME: for some projects no project info available
+            try:
+                self.name = scratchwebapi.request_project_name_for(self.project_id)
+                self.description = scratchwebapi.request_project_description_for(self.project_id)
+            except urllib2.HTTPError:
+                self.name = str(self.project_id)
+                self.description = ""
+        self.name = self.name.strip()
         self.md5_to_resource_path_map = read_md5_to_resource_path_mapping()
         assert self['penLayerMD5'] not in self.md5_to_resource_path_map
         for scratch_object in self.objects:
@@ -174,40 +207,25 @@ class Project(RawProject):
                     listened_keys += script.arguments
         self.listened_keys = set(listened_keys)
         # TODO: rename
-        self.background_md5_names = set([costume[JsonKeys.COSTUME_MD5] for costume in self.stage_object.get_costumes()])
+        self.background_md5_names = set([costume[JsonKeys.COSTUME_MD5] for costume in self.get_costumes()])
         self.unused_resource_names, self.unused_resource_paths = common.pad(zip(*self.find_unused_resources_name_and_filepath()), 2, [])
         for unused_path in self.unused_resource_paths:
             _log.warning("Project folder contains unused resource file: '%s'. These will be omitted for Catrobat project.", os.path.basename(unused_path))
 
     def find_unused_resources_name_and_filepath(self):
-        for file_path in glob.glob(os.path.join(self.project_base_path, "*")):
+        # TODO: remove duplication with __init__
+        for file_path in glob.glob(os.path.join(self.project_base_path, "*.*")):
             md5_resource_filename = common.md5_hash(file_path) + os.path.splitext(file_path)[1]
             if md5_resource_filename not in self.resource_names:
                 if os.path.basename(file_path) != _PROJECT_FILE_NAME:
                     yield md5_resource_filename, file_path
 
-    def find_all_resource_dicts_for(self, resource_name):
-        for resource in self._raw_resources():
-            if resource_name in set([resource.get(JsonKeys.SOUND_MD5), resource.get(JsonKeys.COSTUME_MD5)]):
-                yield resource
-
-
-# TODO: rename
-# TODO: do not use DictAccessWrapper
-class Object(common.DictAccessWrapper):
-
-    def __init__(self, object_data):
-        if not self.is_valid_class_input(object_data):
-            raise ObjectError("Input is no valid Scratch object.")
-        for key in (JsonKeys.SOUNDS, JsonKeys.COSTUMES, JsonKeys.SCRIPTS, JsonKeys.VARIABLES):
-            if key not in object_data:
-                object_data[key] = []
-        super(Object, self).__init__(object_data)
-        self.scripts = [Script(_) for _ in self.get_scripts() if Script.is_valid_script_input(_)]
-
-    @classmethod
-    def is_valid_class_input(cls, object_data):
-        return 'objName' in object_data
+    def find_all_resource_names_for(self, resource_unique_id):
+        resource_names = set()
+        for raw_resource in self._raw_resources():
+            if resource_unique_id in set([raw_resource.get(JsonKeys.SOUND_MD5), raw_resource.get(JsonKeys.COSTUME_MD5)]):
+                resource_names.update([raw_resource[JsonKeys.COSTUME_NAME if JsonKeys.COSTUME_NAME in raw_resource else JsonKeys.SOUND_NAME]])
+        return list(resource_names)
 
 
 # TODO: rename

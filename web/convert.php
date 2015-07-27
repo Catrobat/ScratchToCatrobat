78640d6ec4fdde9aa7adf8a02ea8a70183bb6b42
Ralph Samer <ralph.samer@student.tugraz.at>
2015-07-28 00:32:09 +0200

    refactored converter php script

--- web/convert.php
+++ web/convert.php
@@ -1,51 +1,73 @@
 <?php
 
-# Settings
-//DEFINE('JYTHON_EXE', '/opt/jython27/jython');
-DEFINE('JYTHON_EXE', '/Users/r4ll3/Development/Desktop/jython2.7b/bin/jython');
-DEFINE('S2C_MAIN', '-m scratchtobat.main');
+//------------------------------------------------------------------------------
+// Parameter validation
+//------------------------------------------------------------------------------
 
-###########################################
+$tmpNameNotGiven = (! isset($_FILES["filename"]["tmp_name"])
+                 || empty($_FILES["filename"]["tmp_name"]));
+$URLNotGiven = (! isset($_POST['url']) || empty($_POST['url']));
 
-if ((!isset($_FILES["filename"]["tmp_name"]) || empty($_FILES["filename"]["tmp_name"])) && 
-      (!isset($_POST['url']) || empty($_POST['url']))) {
-	error_log("Files: ".print_r($_FILES, true));
-	error_log("Post: ".print_r($_POST, true));
-  die("No file selected to convert!");
+if ($tmpNameNotGiven && $URLNotGiven) {
+    error_log("Files: ".print_r($_FILES, true));
+    error_log("Post: ".print_r($_POST, true));
+    die("No file selected to convert!");
 }
 
-$file_or_url = isset($_POST['url']) ? $_POST['url'] : $_FILES["filename"]["tmp_name"];
-//$cmd = JYTHON_EXE.' '.S2C_MAIN.' '.escapeshellcmd($file_or_url).' '.$output_dir;
-if (isset($_POST['url'] && strpos($file_or_url, 'http://scratch.mit.edu/projects/') === false) {
+$fileOrURL = isset($_POST['url']) ? $_POST['url'] : $_FILES["filename"]["tmp_name"];
+
+if (isset($_POST['url'])
+&& strpos($fileOrURL, 'http://scratch.mit.edu/projects/') === false
+&& strpos($fileOrURL, 'https://scratch.mit.edu/projects/') === false)
+{
 	die("Invalid URL given!");
 }
-$output_dir = '../output/';
-$cmd = '../converter '.escapeshellcmd($file_or_url).' '.$output_dir;
-$sys_rv = exec($cmd);
 
-$output_files = glob($output_dir.DIRECTORY_SEPARATOR."*.catrobat");
-if ($sys_rv == 1 || count($output_files) != 1) {
+
+//------------------------------------------------------------------------------
+// Conversion
+//------------------------------------------------------------------------------
+
+ignore_user_abort(true);
+
+$outputDir = '../web_output/';
+$outputFiles = glob($outputDir.DIRECTORY_SEPARATOR."*.catrobat");
+foreach ($outputFiles as $oldOutputFile) { // iterate files
+    if (is_file($oldOutputFile)) {
+        unlink($oldOutputFile); // delete file
+	}
+}
+exit();
+
+$cmd = '../converter ' . escapeshellcmd($fileOrURL) . ' ' . $outputDir;
+$sysRV = exec($cmd);
+
+$outputFiles = glob($outputDir.DIRECTORY_SEPARATOR."*.catrobat");
+if (($sysRV == 1) || (count($outputFiles) == 0)) {
 	die("Conversion failed!");
 }
 
-# force download
-$zip_file = $output_files[0];
-$file_name = basename($zip_file);
-error_log("Zipfile: ".$zip_file." basename: ".$file_name);
+
+//------------------------------------------------------------------------------
+// Download
+//------------------------------------------------------------------------------
+
+$zipFile = $outputFiles[0];
+$fileName = basename($zipFile);
+error_log("Zipfile: " . $zipFile . " basename: " . $fileName);
 
 header("Content-Type: application/catrobat+zip");
 header("Content-Disposition: attachment; filename=$file_name");
-header("Content-Length: " . filesize($zip_file));
+header("Content-Length: " . filesize($zipFile));
+readfile($zipFile);
 
-readfile($zip_file);
-
-ignore_user_abort(true);
 if (connection_aborted()) {
-  $temp_files = glob($output_dir.'/{,.}*', GLOB_BRACE);
-	error_log("temp_files to delete: ".print_r($temp_files, true));
-  foreach ($temp_files as $file) { // iterate files
-		if(is_file($file)) {
-			unlink($file); // delete file
+    $temp_files = glob($outputDir . '/{,.}*', GLOB_BRACE);
+    error_log("temp_files to delete: " . print_r($temp_files, true));
+
+    foreach ($temp_files as $file) { // iterate files
+        if (is_file($file)) {
+            unlink($file); // delete file
 		}
-	} 
+	}
 }

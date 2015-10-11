93438f504872ecffcd48f218262fac8a0c35dc32
Ralph Samer <ralph.samer@student.tugraz.at>
2015-10-11 21:42:00 +0200

    new "add text to image" + "save edited image" helper functions

--- src/scratchtocatrobat/tools/image_processing.py
+++ src/scratchtocatrobat/tools/image_processing.py
@@ -20,15 +20,64 @@
 #  along with this program.  If not, see http://www.gnu.org/licenses/.
 
 import logging
-#from java.awt import Graphics
+import os
+from java.io import File
 from javax.imageio import ImageIO
+from java.awt.image import BufferedImage
+from java.awt import Font
+from java.awt import Color
+import imghdr
+from scratchtocatrobat import common
 
 log = logging.getLogger(__name__)
+_supported_image_file_types = ['gif', 'jpeg', 'png']
+_supported_fonts_path_mapping = { 'Helvetica' : {
+    'regular': 'nimbus-sans-l/nimbus-sans-l_regular.ttf',
+    'bold': 'nimbus-sans-l/nimbus-sans-l_bold.ttf',
+    'italic': 'nimbus-sans-l/nimbus-sans-l_italic.ttf',
+    'bold-italic': 'nimbus-sans-l/nimbus-sans-l_bold-italic.ttf'
+}}
 
-def add_text_to_image(image, text, font, size, x, y):
-    assert isinstance(image, ImageIO)
-    g = image.getGraphics()
-    g.setFont(g.getFont().deriveFont(30.0))
+def read_editable_image_from_disk(path):
+    assert os.path.isfile(path)
+    assert imghdr.what(path) in _supported_image_file_types
+    return ImageIO.read(File(path))
+
+def create_font(font_name, size, bold=False, italic=False):
+    assert font_name in _supported_fonts_path_mapping
+    assert isinstance(size, float)
+
+    font_base_path = os.path.join(common.get_project_base_path(), 'resources', 'fonts')
+    fonts = _supported_fonts_path_mapping[font_name]
+    if bold and italic:
+        font_path = os.path.join(font_base_path, fonts['bold-italic'])
+    elif bold:
+        font_path = os.path.join(font_base_path, fonts['bold'])
+    elif italic:
+        font_path = os.path.join(font_base_path, fonts['italic'])
+    else:
+        font_path = os.path.join(font_base_path, fonts['regular'])
+    assert os.path.isfile(font_path)
+
+    font = Font.createFont(Font.TRUETYPE_FONT, File(font_path))
+    return font.deriveFont(size)
+
+def add_text_to_image(editable_image, text, font, color, x, y):
+    assert isinstance(editable_image, BufferedImage), "No *editable* image (instance of ImageIO) given!"
+    assert len(text) > 0, "No or empty text given..."
+    assert isinstance(font, Font), "No valid font given! Should be instance of java.awt.Font!"
+    assert isinstance(color, Color), "No valid color given! Should be instance of java.awt.Color!"
+    assert isinstance(x, float)
+    assert isinstance(y, float)
+
+    g = editable_image.getGraphics()
+    g.setFont(font)
+    g.setColor(color)
     g.drawString(text, x, y)
     g.dispose()
-    return image
+    return editable_image
+
+def save_editable_image_as_png_to_disk(editable_image, path, overwrite=False):
+    assert isinstance(editable_image, BufferedImage), "No *editable* image (instance of ImageIO) given!"
+    assert overwrite == True or os.path.isfile(path) == False, "File already exists"
+    ImageIO.write(editable_image, "png", File(path))

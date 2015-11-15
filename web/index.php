a70965428d9b6d61f91786704ba1b8dad24fc141
Ralph Samer <ralph.samer@student.tugraz.at>
2015-11-15 20:17:34 +0100

    added spinner animation to webpage

--- web/index.php
+++ web/index.php
@@ -25,7 +25,33 @@ $buildNumber = valueForConfigKey('build_number:', $rawConfigContent);
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
   <script type="text/javascript" src="./js/qrcode.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="./js/spin.min.js"></script>
   <script>
+    // var opts = {
+    //   lines: 13, // The number of lines to draw
+    //   length: 28, // The length of each line
+    //   width: 14, // The line thickness
+    //   radius: 42, // The radius of the inner circle
+    //   scale: 1, // Scales overall size of the spinner
+    //   corners: 1, // Corner roundness (0..1)
+    //   color: '#000', // #rgb or #rrggbb or array of colors
+    //   opacity: 0.25, // Opacity of the lines
+    //   rotate: 0, // The rotation offset
+    //   direction: 1, // 1: clockwise, -1: counterclockwise
+    //   speed: 1, // Rounds per second
+    //   trail: 60, // Afterglow percentage
+    //   fps: 20, // Frames per second when using setTimeout() as a fallback for CSS
+    //   zIndex: 2e9, // The z-index (defaults to 2000000000)
+    //   className: 'spinner', // The CSS class to assign to the spinner
+    //   top: '50%', // Top position relative to parent
+    //   left: '50%', // Left position relative to parent
+    //   shadow: false, // Whether to render a shadow
+    //   hwaccel: false, // Whether to use hardware acceleration
+    //   position: 'absolute' // Element positioning
+    // };
+    // var target = document.getElementById('foo');
+    // var spinner = new Spinner(opts).spin(target);
+
     function getProjectIDFromURL(projectURL) {
         if (projectURL == null) {
           return null;
@@ -95,7 +121,7 @@ $buildNumber = valueForConfigKey('build_number:', $rawConfigContent);
         }
   		});
   		$("#converter_form").submit(function(event) {
-        var projectConversionURL = "/api/v1/convert.php?id={0}";
+        var projectConversionURL = "/api/v1/start_conversion.php?id={0}";
         var projectID = getProjectIDFromURL($("#field-url").val());
         $("#field-url-validation-result").html("");
         if (projectID == null) {
@@ -105,7 +131,8 @@ $buildNumber = valueForConfigKey('build_number:', $rawConfigContent);
           event.preventDefault();
           return false;
         }
-        $.getJSON(projectConversionURL, function(data) {
+        $.getJSON(projectConversionURL.replace("{0}", projectID), function(data) {
+
         });
         event.preventDefault();
         return false;

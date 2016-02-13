396e68f3d59d47a50a7beaa65826e2289f370cbc
Ralph Samer <ralph.samer@student.tugraz.at>
2016-02-13 05:21:31 +0100

    removed unused import

--- src/scratchtocatrobat/scratchwebapi.py
+++ src/scratchtocatrobat/scratchwebapi.py
@@ -18,7 +18,7 @@
 #
 #  You should have received a copy of the GNU Affero General Public License
 #  along with this program.  If not, see http://www.gnu.org/licenses/.
-import sys, os, re, json
+import sys, os, re
 from urlparse import urlparse
 from scratchtocatrobat import common
 from tools import helpers

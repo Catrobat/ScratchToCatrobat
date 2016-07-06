05a535f9bb4d13800a4cba14eb5401f620afb56b
Ralph Samer <ralph.samer@student.tugraz.at>
2016-07-07 00:46:51 +0200

    STCC-52 Tests for SetTransparencyBrick

--- src/scratchtocatrobat/test_converter.py
+++ src/scratchtocatrobat/test_converter.py
@@ -708,6 +708,17 @@ class TestConvertBlocks(common_testing.BaseTestCase):
         assert formula_right_child.leftChild is None
         assert formula_right_child.rightChild is None
 
+    # setGraphicEffect:to: (ghost)
+    def test_can_convert_set_graphic_effect_ghost_block(self):
+        scratch_block = _, _, expected_value = ["setGraphicEffect:to:", "ghost", 10.2]
+        [catr_brick] = self.block_converter._catrobat_bricks_from(scratch_block, DUMMY_CATR_SPRITE)
+        assert isinstance(catr_brick, catbricks.SetTransparencyBrick)
+        formula_tree_value = catr_brick.getFormulaWithBrickField(catbasebrick.BrickField.TRANSPARENCY).formulaTree # @UndefinedVariable
+        assert formula_tree_value.type == catformula.FormulaElement.ElementType.NUMBER
+        assert str(expected_value) == formula_tree_value.value
+        assert formula_tree_value.leftChild is None
+        assert formula_tree_value.rightChild is None
+
     # showVariable:
     def test_can_convert_show_variable_block(self):
         # create user variable

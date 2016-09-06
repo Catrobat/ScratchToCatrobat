f974cf6e0b9e032e4dce9b009d01fd97b6139c90
Ralph Samer <ralph.samer@student.tugraz.at>
2016-09-07 00:58:20 +0200

    Added missing worker settings file

---
+++ web/workersettings.py
@@ -0,0 +1,29 @@
+#  ScratchToCatrobat: A tool for converting Scratch projects into Catrobat programs.
+#  Copyright (C) 2013-2015 The Catrobat Team
+#  (<http://developer.catrobat.org/credits>)
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as
+#  published by the Free Software Foundation, either version 3 of the
+#  License, or (at your option) any later version.
+#
+#  An additional term exception under section 7 of the GNU Affero
+#  General Public License, version 3, is available at
+#  http://developer.catrobat.org/license_additional_term
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# REDIS_URL = 'redis://localhost:57356/1'
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+# REDIS_DB = 3
+REDIS_PASSWORD = '' # TODO: insert your secret password here!
+
+# Queues to listen on
+QUEUES = ['high', 'normal', 'low', 'default']

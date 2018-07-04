f58b5457d4c8ef784f0a12aaf0f43a663c2a8b67
Philipp Fleischhacker <p.fleischhacker@student.tugraz.at>
2018-07-04 13:06:06 +0200

    basic sound block testcases

--- src/scratchtocatrobat/scratch/scratch3visitor/test_scratch3.py
+++ src/scratchtocatrobat/scratch/scratch3visitor/test_scratch3.py
@@ -237,24 +237,85 @@ class TestScratch3Blocks(unittest.TestCase):
         converted_block = visitBlock(context)
         assert converted_block[0] == "comeToFront"
 
-    def test_visitCostumenumbername(self):
-        context = create_block_context("looks_costumenumbername")
-        testblock = context.block
-        addInputOfType(testblock, "CHANGE", TYPE_INT)
-        testblock.fields["NUMBER_NAME"] = ["name"]
-        testblock.fields["EFFECT"] = ["testeffect"]
-        converted_block = visitBlock(context)
-        assert converted_block[0] == "costumeName"
-
-        testblock.fields["NUMBER_NAME"] = ["number"]
-        converted_block = visitBlock(context)
-        assert converted_block[0] == "costumeIndex"
+    # def test_visitCostumenumbername(self):
+    #     context = create_block_context("looks_costumenumbername")
+    #     testblock = context.block
+    #     addInputOfType(testblock, "CHANGE", TYPE_INT)
+    #     testblock.fields["NUMBER_NAME"] = ["name"]
+    #     testblock.fields["EFFECT"] = ["testeffect"]
+    #     converted_block = visitBlock(context)
+    #     assert converted_block[0] == "costumeName"
+    #
+    #     testblock.fields["NUMBER_NAME"] = ["number"]
+    #     converted_block = visitBlock(context)
+    #     assert converted_block[0] == "costumeIndex"
 
     def test_visitSize(self):
         context = create_block_context("looks_size")
         converted_block = visitBlock(context)
         assert converted_block[0] == "scale"
 
+
+
+
+
+######### Sound Blocks #########################################
+    def add_menu_block(self, context, opcode, menu_name, value):
+        menu_block = add_new_block_to_context(context, opcode)
+        menu_block.fields[menu_name] = [value]
+        context.block.inputs[menu_name] = [1, menu_block.name]
+
+    def test_visitPlay(self):
+        context = create_block_context("sound_play")
+        testblock = context.block
+
+        block2 = add_new_block_to_context(context, "sound_sounds_menu")
+        block2.fields["SOUND_MENU"] = ["test_sound"]
+        # addInputToBlock(testblock, "SOUND_MENU", block2.name, INPUTTYPE_BLOCK_NO_SHADOW)
+        testblock.inputs["SOUND_MENU"] = [1, block2.name]
+
+        converted_block = visitBlock(context)
+        assert converted_block[0] == "playSound:"
+        assert converted_block[1] == "test_sound"
+
+    def test_visitPlayuntildone(self):
+        context = create_block_context("sound_playuntildone")
+        testblock = context.block
+        self.add_menu_block(context, "sound_sounds_menu", "SOUND_MENU", "test_sound")
+        converted_block = visitBlock(context)
+        assert converted_block[0] == "doPlaySoundAndWait"
+        assert converted_block[1] == "test_sound"
+
+    def test_visitStopallsounds(self):
+        context = create_block_context("sound_stopallsounds")
+        converted_block = visitBlock(context)
+        assert converted_block[0] == "stopAllSounds"
+
+    def test_visitClearsoundeffects(self):
+        context = create_block_context("sound_cleareffects")
+        converted_block = visitBlock(context)
+        assert converted_block[0] == "clearSoundEffects"
+
+    def test_visitChangevolumeby(self):
+        context = create_block_context("sound_changevolumeby")
+        testblock = context.block
+        addInputOfType(testblock, "VOLUME", TYPE_INT)
+        converted_block = visitBlock(context)
+        assert converted_block[0] == "changeVolumeBy:"
+        assert converted_block[1] == 1234.0
+
+    def test_visitSetvolumeto(self):
+        context = create_block_context("sound_setvolumeto")
+        testblock = context.block
+        addInputOfType(testblock, "VOLUME", TYPE_INT)
+        converted_block = visitBlock(context)
+        assert converted_block[0] == "setVolumeTo:"
+        assert converted_block[1] == 1234.0
+
+    def test_visitVolume(self):
+        context = create_block_context("sound_volume")
+        converted_block = visitBlock(context)
+        assert converted_block[0] == "volume"
 
 
 if __name__ == "__main__":
